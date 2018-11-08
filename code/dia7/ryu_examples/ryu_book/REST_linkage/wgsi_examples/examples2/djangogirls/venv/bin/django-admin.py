1e54d21ca251be1bb98fa239b00068ab89351a58
Unknown <helpu1409@gmail.com>
2018-11-07 22:43:22 -0500

    remove django girls project

--- code/dia7/ryu_examples/ryu_book/REST_linkage/wgsi_examples/examples2/djangogirls/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/tigarto/Documents/tesis_2018-2/sdn_repaso/code/dia7/ryu_examples/ryu_book/REST_linkage/wgsi_examples/examples2/djangogirls/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
